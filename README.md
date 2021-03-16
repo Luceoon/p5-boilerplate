d81ddadb5c215a4d59d06aa5c383dfa8647cc493
Lucas Jahn <luceoon@gmail.com>
2021-03-16 15:06:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## P5.js Boilerplate
+
+A Boilerplate for p5.js to create a simple sketch.
+
+> Just download, write your code in **sketch.js** and done.
---
+++ sketch.js
@@ -0,0 +1,23 @@
+// called once on load
+function preload() {
+
+  // ++ load data here ++ //
+}
+
+// called once when loaded
+function setup() {
+
+  // init canvas
+  canvas = createCanvas(800, 800).parent('canvas');
+
+  // ++ init stuff here ++ //
+}
+
+// called every frame
+function draw() {
+
+  // background
+  background(0);;
+
+  // ++ draw frame here ++ //
+}
